a541b56b9ec0ee216b52eb0b2e9f90c4e0caf1c7
aaayon91 <aaayon91@gmail.com>
2022-01-14 14:03:00 -0800

    adds Spot model

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-class Spot:
-    def __init__(self, location, type, price):
-        self.location = location
-        self.type = type
-        self.price = price 
-spots = [
-  Spot('LA', 'gated', '100'),
-  Spot('Koreatown', 'outside', '200'),
-  Spot('Torrance', 'street', '300')
-]
 
 # Create your views here.
 def home(request):

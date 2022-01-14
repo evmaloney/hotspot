4dd348d07e5c2ae2ced29e628d1acde1068bea9f
Mohamed <mfarah37@gmail.com>
2022-01-14 15:52:18 -0800

    updates model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,4 @@
+from ctypes import addressof
 import imp
 from django.db import models
 from django.urls import reverse
@@ -14,14 +15,14 @@ TYPES = (
 
 # Create your models here.
 class Spot(models.Model):
-    location = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
     type = models.CharField(
         max_length=15,
         choices=TYPES,
         default=TYPES[3][0]
     )
     price = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.location
+        return self.address

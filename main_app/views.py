b706e1c8046380a4a0ecc062ee949625ab390adf
aaayon91 <aaayon91@gmail.com>
2022-01-15 13:40:43 -0800

    adds spots_detail to urls and views

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Spot
+from .models import Spot, Feature
 from django.contrib.auth.decorators import login_required
 
 
@@ -14,3 +14,16 @@ def about(request):
 def spots_index(request):
   spots = Spot.objects.all()
   return render(request, 'spots/index.html', {'spots': spots})
+
+def spots_detail(request, spot_id):
+  if request.user.spot_set.filter(id=spot_id).exists():
+    spot = Spot.objects.get(id=spot_id)
+    features_spot_doesnt_have = Feature.objects.exclude(id__in = spot.features.all().values_list('id'))
+    # instantiate BookingForm to be rendered in the template
+    booking_form = BookingForm()
+    return render(request, 'spots/detail.html', {
+      'spot': spot, 'booking_form': booking_form,
+      'features': features_spot_doesnt_have
+    })
+  else:
+    return redirect('index')

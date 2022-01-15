913d4b3cfed32c1d34b2ef9aac73756414c87b2f
Mohamed <mfarah37@gmail.com>
2022-01-15 13:27:46 -0800

    adds new models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,5 @@
+import types
+from unicodedata import name
 from django.db import models
 from django.urls import reverse
 from datetime import date
@@ -11,6 +13,15 @@ TYPES = (
     ('Other', 'Other')
 )
 
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('features_detail', kwargs={'pk': self.id})
+
 # Create your models here.
 class Spot(models.Model):
     address = models.CharField(max_length=150)
@@ -20,7 +31,23 @@ class Spot(models.Model):
         default=TYPES[3][0]
     )
     price = models.IntegerField()
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
+    Features = models.ManyToManyField(Feature)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.address
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'spot_id': self.id})
+
+class Bookings(models.Model):
+    startdate = models.DateField('start date')
+    enddate = models.DateField('end date')
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    spot = models.ForeignKey(Spot, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for spot_id: {self.spot_id} @{self.url}"
